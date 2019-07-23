cb66f764cd2d302b492ad03c9503641c636d09d6
reineradalid <reineradalid96@gmail.com>
2019-07-23 08:37:49 +0800

    asd

---
+++ src/components/signup.js
@@ -0,0 +1,27 @@
+import {
+    Form,
+    Input,
+    Tooltip,
+    Icon,
+    Cascader,
+    Select,
+    Row,
+    Col,
+    Checkbox,
+    Button,
+    AutoComplete,
+  } from 'antd';
+  import React from 'react';
+
+const { Option } = Select;
+const AutoCompleteOption = AutoComplete.Option;
+
+  class signup extends React.Component {
+      render() {
+          return (
+             console.log('test')
+          );
+      }
+  }
+  
+  export default signup;
