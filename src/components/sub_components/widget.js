d9b6e1de6948c26d27012ea3887fbc8acbb9dcf5
reineradalid <reineradalid96@gmail.com>
2019-07-24 08:15:20 +0800

    asd

--- src/components/sub_components/widget.js
+++ src/components/sub_components/widget.js
@@ -1,5 +1,14 @@
 import React from 'react';
-import { Card,Icon,Row,Col, Layout, Modal,} from 'antd';
+import {Card,
+        Icon,
+        Row,
+        Col, 
+        Layout, 
+        Modal,
+        Form,
+        Input,
+        Checkbox,
+        Button} from 'antd';
 import Signup from './signup.js';
 
 
@@ -12,7 +21,9 @@ import Signup from './signup.js';
   
 class Widget extends React.Component {
     state = {
+        img:'https://images.pexels.com/photos/67636/rose-blue-flower-rose-blooms-67636.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500',
         visible: false,
+        visibles: false,
         key: 'tab1',
         noTitleKey: 'app',
         icon:'close-circle',
@@ -43,11 +54,15 @@ class Widget extends React.Component {
       };
 
       iconchange = () => {
-        if(this.state.icon == 'close-circle') {
-            this.setState({ icon: 'check-circle' });
-        }else{
-            this.setState({ icon: 'close-circle' });
-        }
+        // if(this.state.icon == 'close-circle') {
+        //     this.setState({ icon: 'check-circle' });
+        // }else{
+        //     this.setState({ icon: 'close-circle' });
+        // }
+        this.state.icon == 'close-circle' ?  
+        this.setState({ icon: 'check-circle' }) 
+        :  
+         this.setState({ icon: 'close-circle' });
        
       };
       showModal = () => {
@@ -55,14 +70,8 @@ class Widget extends React.Component {
           visible: true,
         });
       };
-    
-      handleOk = e => {
-        console.log(e);
-        this.setState({
-          visible: false,
-        });
-      };
-    
+     
+     
       handleCancel = e => {
         console.log(e);
         this.setState({
@@ -72,13 +81,15 @@ class Widget extends React.Component {
   
     render() {
         return (
-            <div style={{margin:2, borderRadius:20}}>
-                <Layout  style={{ maxHeight: '50vh'  }} >
+            <div style={{marginLeft:2}} id='cardH'>
+                <Layout  >
                 <Card
                     style={{ alignItems:'center', 
                             justifyContent:'center', 
                             width:'100%', 
-                            borderRadius:10, 
+                            borderRadius:10,
+                            maxHeight: '200vh',
+                            maxWidth:'60vh' 
                             }}
                             title={<h1 style={{fontSize:25}}>{this.state.title}</h1>}
                     // tabList={tabList}
@@ -89,8 +100,9 @@ class Widget extends React.Component {
                   
                     >
                     {/* {contentList[this.state.key]} */}
+                    {/* <Signup/> */}
                     <div >
-                        <img style={{width:'100%'}} src={'https://images.pexels.com/photos/814499/pexels-photo-814499.jpeg?auto=compress&cs=tinysrgb&dpr=1&w=500'} />
+                        <img style={{width:'100%'}} src={this.state.img} />
                     </div>
                     <div style={{margin:20}}>
                         <div style={{textAlign:'left', color:'gray', fontSize:15}}>
@@ -127,15 +139,59 @@ class Widget extends React.Component {
                 </Card>
                 </Layout>
                 <Modal
-                    title="Basic Modal"
+                   
                     visible={this.state.visible}
-                    onOk={this.handleOk}
-                    onCancel={this.handleCancel}
+                    // onOk={this.handleOk}
+                   onCancel={this.handleCancel}
+                    footer={[
+                      null,
+                      null,
+                    ]}
+                    style={{ maxHeight: '20vh',
+                            maxWidth:'40vh' }}
                     >
-                    <div visible={this.state.visible} style={{alignItems:'center', justifyContent:'center'}} >
-                        <Signup/>
-                    </div>
+                    
+                    <Form onSubmit={this.handleSubmit} className="login-form" style={{width: '100%', alignItems:'center', justifyContent:'center',marginTop:50}}>
+                      <Form.Item >
+                      
+                          <Input
+                          prefix={<Icon type="user" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                          placeholder="Username"
+                          
+                          />
+                      </Form.Item>
+                      <Form.Item >
+                      
+                          <Input
+                          prefix={<Icon type="lock" style={{ color: 'rgba(0,0,0,.25)' }} />}
+                          type="password"
+                          placeholder="Password"
+                      
+                          />
+                      </Form.Item>
+                      <Form.Item >
+                        <Checkbox style={{float:'left'}}>Remember me</Checkbox>
+                        <Button style={{float:'right'}}  type="primary" htmlType="submit" className="login-form-button">
+                            Log in
+                        </Button>
+                        
+                      </Form.Item>
+                      <Form.Item >
+                        
+                        <a style={{float:'right'}} className="login-form-forgot" href="">
+                            Forgot password
+                        </a>
+                    
+                      </Form.Item>
+                     
+                      <Form.Item style={{float:'right', marginTop:-30}}>
+                          Or <a href=""> register now!</a>
+                      </Form.Item>
+                    </Form>
+                    
                 </Modal>
+
+                
             </div>
 
 
