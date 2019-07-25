a6dbb61075b070e2060c639c197c0119ebe0fba9
hambog00 <hambog00@gmail.com>
2019-07-25 09:45:57 +0800

    Woking function

--- src/components/backend/crud.js
+++ src/components/backend/crud.js
@@ -1,31 +1,34 @@
 
-module.exports = {
+export function getRecord() {
 
-    getRecord: function(){
-        
-        fetch('http://172.104.62.175:1338/leads/classes/MLBB', {
-            method: 'GET',
-            headers: {
-              'Content-Type': 'aplication/json',
-              'X-Parse-Application-Id': 'PARSE17210462175'
-            }
-          })
-          .then(res => res.json())
-          .then((result) => {
-                  
-                  return result;
-                },
-                (error) => {
-                  console.log(error);
-                }
-          )
+  // fetch('http://172.104.62.175:1338/leads/classes/MLBB', {
+  //   headers: {
+  //     'Content-Type': 'aplication/json',
+  //     'X-Parse-Application-Id': 'PARSE17210462175'
+  //   }
+  // })
+  // .then(res => res.json() )
+  // .then((result) => {
+  //     return 'YEASSIR';
+  //   },
+  //   (error) => {
+  //     console.log(error);
+  //   }
+  // )
 
+  return fetch('http://172.104.62.175:1338/leads/classes/MLBB',{
 
-    },
- 
-    setRecord: function(){
-        return 'SOmething';
-    }
- 
- }
+    headers: {
+          'Content-Type': 'aplication/json',
+          'X-Parse-Application-Id': 'PARSE17210462175'
+        }
+    })
+     .then((data)=>data.json())
+     .then((res)=>res)
+
+}
+
+export function testing(){
+  return 'YEAH'
+}
 
