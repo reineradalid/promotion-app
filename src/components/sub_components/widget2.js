965ca928c2a650252fe02968ac4f394b7ed1030b
reineradalid <reineradalid96@gmail.com>
2019-07-25 08:56:34 +0800

    asd

--- src/components/sub_components/widget2.js
+++ src/components/sub_components/widget2.js
@@ -93,70 +93,73 @@ class Widget2 extends React.Component {
   
     render() {
         return (
-            <div  style={{marginLeft:2, flex:1, alignItems:'center', justifyContent:'center '}} id='cardH'>
-                <Layout  >
-                <Card
-                    style={{ alignItems:'center', 
-                            justifyContent:'center', 
-                            width:'100%', 
-                            borderRadius:10,
-                            maxHeight: '200vh',
-                            maxWidth:'60vh' 
-                            }}
-                            title={<h1 style={{fontSize:25}}>{this.state.title}</h1>}
-                    // tabList={tabList}
-                    activeTabKey={this.state.key}
-                    onTabChange={key => {
-                        this.onTabChange(key, 'key');
-                    }}
-                  
-                    >
-                    {/* {contentList[this.state.key]} */}
-                    {/* <Signup/> */}
-                    <div >
-                        <img style={{width:'100%'}} src={this.state.img} />
-                    </div>
-                    <div style={{margin:20}}>
-                        <div style={{textAlign:'left', color:'gray', fontSize:15}}>
-                            {this.items = this.state.description.map((item, key) =>
-                                <li key={item.id}>&bull; {item.des}</li>
-                            )}                          
-                        </div>
-                    </div>
-                    <div style={{marginBottom:10}}>
-                        <div style={{textAlign:'left', color:'gray'}}>
-                           
-                            <a  >
-                              <div style={{marginBottom:10}}>  
-                                <Row   type="flex" justify="space-around" align="middle"  style={{border:'1px solid gray',borderRadius:5 }}>
-                                  <Col   span={20}><h2 style={{ float:'left', marginLeft:20}}> Sign in </h2></Col> 
-                                  <Col style={{float:'right'}}   span={2}><Icon style={{fontSize:20, float:'right'}}  type={this.state.icon} /></Col>               
-                                </Row>
-                                </div>
+            <div  style={{ flex:1, alignItems:'center', justifyContent:'center ', overflowY:'hidden'}} id='cardH'>
+              <div style={{ flex:1, alignItems:'center', justifyContent:'center ', marginTop:30,marginBottom:30, overflowX:'hidden', overflowY:'hidden'}} id='cardH'>     
+                <Col  type="flex" justify="center" align="middle">
+                  <Card
+                      style={{ alignItems:'center', 
+                              justifyContent:'center', 
+                              width:'100%', 
+                              borderRadius:10,
+                              maxHeight: '200vh',
+                              maxWidth:'60vh' 
+                              }}
+                              title={<h1 style={{fontSize:25}}>{this.state.title}</h1>}
+                      // tabList={tabList}
+                      activeTabKey={this.state.key}
+                      onTabChange={key => {
+                          this.onTabChange(key, 'key');
+                      }}
+                    
+                      >
+                      {/* {contentList[this.state.key]} */}
+                      {/* <Signup/> */}
+                      <div >
+                          <img style={{width:'100%'}} src={this.state.img} />
+                      </div>
+                      <div style={{margin:20}}>
+                          <div style={{textAlign:'left', color:'gray', fontSize:15}}>
+                              {this.items = this.state.description.map((item, key) =>
+                                  <li key={item.id}>&bull; {item.des}</li>
+                              )}                          
+                          </div>
+                      </div>
+                      <div style={{marginBottom:10}}>
+                          <div style={{textAlign:'left', color:'gray'}}>
+                            
+                              <a  >
                                 <div style={{marginBottom:10}}>  
-                                <Row   type="flex" justify="space-around" align="middle"  style={{border:'1px solid gray',borderRadius:5 }}>
-                                  <Col   span={20}><h2 style={{ float:'left', marginLeft:20}}> Download </h2></Col> 
-                                  <Col style={{float:'right'}}   span={2}><Icon style={{fontSize:20, float:'right'}}  type={this.state.icon} /></Col>               
-                                </Row>
-                                </div>
-                                <div style={{marginBottom:10}}>  
-                                <Row   type="flex" justify="space-around" align="middle"  style={{border:'1px solid gray',borderRadius:5 }}>
-                                  <Col   span={20}><h2 style={{ float:'left', marginLeft:20}}>Like Facebook page</h2></Col> 
-                                  <Col style={{float:'right'}}   span={2}><Icon style={{fontSize:20, float:'right'}}  type={this.state.icon} /></Col>               
-                                </Row>
+                                  <Row   type="flex" justify="space-around" align="middle"  style={{border:'1px solid gray',borderRadius:5 }}>
+                                    <Col   span={20}><h2 style={{ float:'left', marginLeft:20}}> Sign in </h2></Col> 
+                                    <Col style={{float:'right'}}   span={2}><Icon style={{fontSize:20, float:'right'}}  type={this.state.icon} /></Col>               
+                                  </Row>
+                                  </div>
+                                  <div style={{marginBottom:10}}>  
+                                  <Row   type="flex" justify="space-around" align="middle"  style={{border:'1px solid gray',borderRadius:5 }}>
+                                    <Col   span={20}><h2 style={{ float:'left', marginLeft:20}}> Download </h2></Col> 
+                                    <Col style={{float:'right'}}   span={2}><Icon style={{fontSize:20, float:'right'}}  type={this.state.icon} /></Col>               
+                                  </Row>
+                                  </div>
+                                  <div style={{marginBottom:10}}>  
+                                  <Row   type="flex" justify="space-around" align="middle"  style={{border:'1px solid gray',borderRadius:5 }}>
+                                    <Col   span={20}><h2 style={{ float:'left', marginLeft:20}}>Like Facebook page</h2></Col> 
+                                    <Col style={{float:'right'}}   span={2}><Icon style={{fontSize:20, float:'right'}}  type={this.state.icon} /></Col>               
+                                  </Row>
 
-                              </div>
-                              <div style={{marginBottom:10}}>  
-                                <Row   type="flex" justify="space-around" align="middle"  style={{border:'1px solid gray',borderRadius:5 }}>
-                                  <Col   span={20}><h2 style={{ float:'left', marginLeft:20}}>Share Facebook page</h2></Col> 
-                                  <Col style={{float:'right'}}   span={2}><Icon style={{fontSize:20, float:'right'}}  type={this.state.icon} /></Col>               
-                                </Row>
-                              </div>
-                            </a>                    
-                        </div>
-                    </div>        
-                </Card>
-                </Layout>
+                                </div>
+                                <div style={{marginBottom:10}}>  
+                                  <Row   type="flex" justify="space-around" align="middle"  style={{border:'1px solid gray',borderRadius:5 }}>
+                                    <Col   span={20}><h2 style={{ float:'left', marginLeft:20}}>Share Facebook page</h2></Col> 
+                                    <Col style={{float:'right'}}   span={2}><Icon style={{fontSize:20, float:'right'}}  type={this.state.icon} /></Col>               
+                                  </Row>
+                                </div>
+                              </a>                    
+                          </div>
+                      </div>        
+                  </Card>
+                </Col>
+               
+              </div>            
                 <Modal
                     visible={this.state.visible}
                     // onOk={this.handleOk}
